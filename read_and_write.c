a074e65cd83bed9dc417ab5b8c588588c5425ba4
KOUDAI <KOUDAI@DESKTOP-G5J27NJ>
2021-01-09 21:44:13 +0900

    refact

--- read_and_write.c
+++ read_and_write.c
@@ -1,46 +1,59 @@
 #include <stdio.h>
 #include <windows.h>
 
-#include "shared.h"
+typedef struct _my_struct
+{
+    unsigned char len;
+    char buffer[0x39];
+} my_struct;
+
+int main_loop(my_struct* pshared)
+{
+    struct _buf {
+        unsigned char mode;
+        char text[0x39];
+    } buf;
+
+    memset(&buf, 0, sizeof(buf));
+    printf("->"); scanf("%s", &buf);
+
+    if (buf.mode == 'r')
+    {
+        printf("%d::%s\n", pshared->len, pshared->buffer);
+        return 1;
+    }
+    else if (buf.mode == 'w')
+    {
+        pshared->len++;
+        strcpy(pshared->buffer, buf.text);
+        return 1;
+    }
+    else
+    {
+        // exit
+        return 0;
+    }
+}
 
 int main()
 {
     puts("hello read_and_write");
 
-    const int kSharedSize = 0x80;
+    const int kSharedSize = sizeof(my_struct);
+    const char* key = "read_and_write";
 
-    // open_or_create
-    HANDLE hmapping = OpenFileMapping(FILE_MAP_READ | FILE_MAP_WRITE, FALSE, "read_and_write");
+    // 自分が最初なら作る
+    HANDLE hmapping = OpenFileMapping(FILE_MAP_READ | FILE_MAP_WRITE, FALSE, key);
     if (hmapping == NULL)
     {
-        hmapping = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, kSharedSize, "read_and_write");
+        hmapping = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, kSharedSize, key);
     }
 
     // 実際にファイルの内容と対応するメモリ領域を取得する
-    hello_message *pshared = MapViewOfFile(hmapping, FILE_MAP_ALL_ACCESS, 0, 0, kSharedSize);
-    
-    unsigned char buf[0x10];
+    my_struct *pshared = MapViewOfFile(hmapping, FILE_MAP_ALL_ACCESS, 0, 0, kSharedSize);
 
-    while (1)
-    {
-        memset(buf, 0, sizeof(buf));
-        printf("message: "); scanf("%s", buf);
-
-        if (buf[0] == 'r')
-        {
-            printf("%d:%s\n",
-                pshared->len, pshared->text);
-        }
-        else if (buf[0] == 'w')
-        {
-            pshared->len++;
-            strcpy(pshared->text, buf);
-        }
-        else 
-        {
-            break;
-        }
-    }
+    // main loop
+    while (main_loop(pshared));
 
     puts("exit app.");
 
